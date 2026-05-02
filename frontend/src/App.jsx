5655b19e0b0376bb9e1d2d533c43c0e24e413ab8
lovelu99 <lovelu@gmail.com>
2026-05-02 14:07:16 -0400

    images tag fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>3-Tier Azure Demo App</h1>
+      <h1>3-Tier Azure Devops Demo App</h1>
       <p>React frontend + ASP.NET Core API + Azure SQL + Key Vault</p>
 
       {error && <p>{error}</p>}
